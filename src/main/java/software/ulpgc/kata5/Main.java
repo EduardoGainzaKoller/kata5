89efb3f1eeabd936ef8f8869ce9eb4d768e6e470
Eduardo <edaniel2004gainza@gmail.com>
2024-11-25 19:09:30 +0000

    Kata 5 final implementation

---
+++ src/main/java/software/ulpgc/kata5/Main.java
@@ -0,0 +1,20 @@
+package software.ulpgc.kata5;
+
+import software.ulpgc.kata5.io.RandomUserAdapter;
+import software.ulpgc.kata5.io.RandomUserDeserializer;
+import software.ulpgc.kata5.io.RandomUserReader;
+import software.ulpgc.kata5.io.UserLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        UserLoader loader = new UserLoader(
+                new RandomUserReader(),
+                new RandomUserDeserializer(),
+                new RandomUserAdapter()
+        );
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println(loader.get().name());
+        }
+    }
+}
